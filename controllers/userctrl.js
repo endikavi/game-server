5b43445262a24bae90a9713266525b234d1acfe7
endikavi <endika.vi@gmail.com>
2018-05-10 16:20:05 +0200

    mejoradas rutas hacer pruebas

--- controllers/userctrl.js
+++ controllers/userctrl.js
@@ -5,10 +5,11 @@ const User = require('../models/user-schema');
 exports.addUserSavedata = (req, res) => {
     
     console.log('Intento de añadir usuario');
-	console.log(req.body.UserConf);
     const NewUser = new User();
+    
     Object.assign(NewUser, req.body);
 	NewUser.UserConf = JSON.parse(req.body.UserConf);
+    
     NewUser.save()
 
         .then(user => {
@@ -16,12 +17,14 @@ exports.addUserSavedata = (req, res) => {
             console.log('Añadido usuario:');
             console.log(user);
 			res.send(user);
+        
         })
         .catch(error => {
             
 			console.log(error);
             console.log('Fallo al añadir usuario');
-			res.send('Fallo al añadir usuario');
+			res.send(false);
+        
         })
 	
 }
@@ -31,15 +34,16 @@ exports.addUserSavedata = (req, res) => {
 exports.deleteUserSavedata = (req, res) => {
 	
 	console.log('Intento de eliminar usuario');
-    console.log(req.params.id);
 	
-	User.remove({
-		uid: req.params.id
-	}, function (err) {
-		if (err) return handleError(err);
+	User.remove({ uid: req.params.id }, function (err) {
+        
+        if (err) return handleError(err);
+        
+        console.log('Usuario eliminado uid: ' + req.params.id);
+        res.send(true);
+        
 	});
-	console.log('Usuario eliminado uid: ' + req.params.id);
-	res.send('Usuario eliminado uid: ' + req.params.id);
+    
 }
 
 //Actualizar info de un usuario y su save
@@ -48,41 +52,37 @@ exports.updateUserSavedata = (req, res) => {
 	
 	console.log('Intento de actualizar partida');
     console.log(req.body);
-	/*const Update = ({
-		UserConf: req.body.UserConf,
-	});
+    
+	const Update = ({ UserConf: req.body.UserConf });
 
-	User.update({
-		Email: req.body.Email
-	}, Update, function (err) {
+	User.update({ uid: req.body.uid }, Update, function (err) {
+        
 		if (err) return handleError(err);
+        
+        console.log('Actualizada partida');
+        res.send(true);
+        
 	});
-
-	console.log('Usuario actualizado');
-
-	res.send('Usuario actualizado')*/
-	res.send('');
+    
 }
+
+//Actualizar uid de un usuario y recuperar su save
 
 exports.updateUserUid = (req, res) => {
 	
 	console.log('Intento de actualizar Uid');
     console.log(req.body);
 	
-	/*const Update = ({
-		UserConf: req.body.UserConf,
-	});
+	const Update = ({ uid: req.body.oldUid });
 
-	User.update({
-		Email: req.body.Email
-	}, Update, function (err) {
+	User.update({ uid: req.body.uid }, Update, function (err) {
+        
 		if (err) return handleError(err);
+        console.log('Actualizado uid');
+        res.send(users);
+        
 	});
-
-	console.log('Usuario actualizado');
-
-	res.send('Usuario actualizado')*/
-	res.send('');
+    
 }
 
 //Recuperar info de un usuario y su save
@@ -91,13 +91,13 @@ exports.getUserSavedata = (req, res) => {
 	
     console.log('Intento de obtener partida guardada');
  	console.log(req.params.id);
-    /*
-	User.find({
-		uid: req.params.id
-	}).lean().exec(function (err, users) {
+    
+	User.find({ uid: req.params.id }).lean().exec(function (err, users) {
+        
 		if (err) return console.error(err);
-		console.log('Busqueda de usuarios realizada,resultados obtenidos ' + users.length + ' usuarios');
-		res.send('{"users":' + JSON.stringify(users) + '}');
-	})*/
-	res.send('');
+		console.log('SaveData obtenido');
+		res.send(users);
+        
+	})
+    
 }
