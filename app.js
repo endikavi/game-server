d73ea6be3fd35e192af70603f223b148eb70a935
endikavi <endika.vi@gmail.com>
2018-05-11 19:01:35 +0200

    EUERYTYERYEY

--- app.js
+++ app.js
@@ -11,12 +11,10 @@ const urlencodedParser = bodyParser.urlencoded({extended: false})
 //socket.io
 
 const http = require('http').Server(app);
-
-http.listen(4000, function(){
-  console.log('listening on :8080');
-});
-
 const io = require('socket.io')(http);
+http.listen(8080);
+
+
 
 //io.origins('*');
 /*8080, {
