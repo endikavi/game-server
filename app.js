d50370212c88167218b518f5e9d42504f3cf1bcd
endikavi <endika.vi@gmail.com>
2018-05-11 16:54:27 +0200

    hjkljhlkl

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use(function(req, res, next) {
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 })
