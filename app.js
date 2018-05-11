4991f5de3a7dc71cacb5112e5881e8a3f465640b
endikavi <endika.vi@gmail.com>
2018-05-11 19:05:52 +0200

    rsgfdgds

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.raw();
 const urlencodedParser = bodyParser.urlencoded({extended: false})
-
+NODE_ENV=production;
 //socket.io
 
 const http = require('http').Server(app);
