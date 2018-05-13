5d56cd874a66043544ac411c6eeb4a26bab78353
endikavi <endika.vi@gmail.com>
2018-05-14 00:55:12 +0200

    tyr

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -14,14 +14,14 @@ function multiplayer(){
     socket.on('walking', function(msg){
         
         console.log('cambiando posicion');
-
+		/*
 		if (player.tileFrom != msg && moving){
 			
 			player.tileTo[0]=msg[0];
 			player.tileTo[1]=msg[1];
 			player.timeMoved = gameTime;
 			moving = false;
-		}
+		}*/
         
     })
     
