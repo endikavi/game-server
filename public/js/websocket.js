825019f770d4049dd9ace4cc6b2381eda7ef812c
endikavi <endika.vi@gmail.com>
2018-05-21 12:37:20 +0200

    ghgfh

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -101,7 +101,7 @@ function printRoom(id,msg){
 	        
     $$('#GCmessages').append('<li><div class="item-content"><div class="item-inner resizable"><div class="item-title">Sala '+ id +':<div class="item-header"><p class="popup-text">'+msg.chief+' </p></div><div class="item-footer">'+msg.people.length+'/4 </div></div><div class="item-after"><button type="button" class="button col button-round btn color-white"id="'+ id +'">Entrar</button></div></div></div></li>');
     
-    $$('#' + id).on('click',function(){
+    $$('#' + id).on('click',function(this){
         
         console.log(this.attr('id'));
         //UserConf[1].roomid = $$('#m').val();
