1850928e38137331795311f15b14f86ccc45fb4b
endikavi <endika.vi@gmail.com>
2018-05-21 11:50:05 +0200

    ghjghf

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -72,7 +72,7 @@ function multiplayer(){
         console.log(msg);
         rooms.list[msg[0]] = msg[1]
         if(UserConf[1].roomid == undefined){
-            printRoom(msg);
+            printRoom(msg[0],msg[1]);
         }
 	})
     
@@ -97,13 +97,11 @@ function printRoomChat(msg){
 	
 }
 
-function printRoom(msg){
+function printRoom(id,msg){
 	        
-    console.log(msg)
+    $$('#GCmessages').append('<li><div class="item-content"><div class="item-inner resizable"><div class="item-title">Sala '+ id +':<div class="item-header"><p class="popup-text">'+msg.chief+' </p></div><div class="item-footer">'+msg.people.length+'/4 </div></div><div class="item-after"><button type="button" class="button col button-round btn color-white"id="'+ id +'">Entrar</button></div></div></div></li>');
     
-    $$('#GCmessages').append('<li><div class="item-content"><div class="item-inner resizable"><div class="item-title">Sala '+ msg[0] +':<div class="item-header"><p class="popup-text">'+msg[1].chief+' </p></div><div class="item-footer">'+msg[1].people.length+'/4 </div></div><div class="item-after"><button type="button" class="button col button-round btn color-white"id="'+ msg[0] +'">Entrar</button></div></div></div></li>');
-    
-    $$('#'+msg[0]).on('click',function(){
+    $$('#' + id).on('click',function(){
         
         console.log(this.attr('id'));
         //UserConf[1].roomid = $$('#m').val();
