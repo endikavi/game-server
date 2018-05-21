78a671df5c33470e3c8b15ba32ef0c8e928a161c
endikavi <endika.vi@gmail.com>
2018-05-21 15:49:48 +0200

    rty

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -21,7 +21,7 @@ function multiplayer(){
     
     socket.on('walking', function(msg){
         
-        console.log('cambiando posicion');
+        console.log('un jugador cambiando posicion');
         
     })
 	
@@ -93,6 +93,14 @@ function multiplayer(){
         
 	})
     
+    socket.on('startGame', function(msg){
+        
+        console.log('Comenzando coperativo');
+        
+        addGameCanvas();
+        
+	})
+    
 }
 
 function printGlobalChat(msg){
@@ -124,9 +132,9 @@ function printYourRoom(msg){
     
     $$('#GCmessages').html('<li><div class="item-content"><div class="item-inner resizable"><div class="item-title">Sala '+UserConf[1].roomid+':<div class="item-header"><p class="popup-text">'+msg.chief+' </p></div><div class="item-footer">'+msg.people.length+'/4 </div></div><div class="item-after"><button type="button" class="button col button-round btn color-white"id="startMG">Empezar</button></div></div></div></li>');
     
-    $$('#startMG').on('click',function (e){
+    $$('#startMG').on('click',function (){
         
-        startMG();
+        socket.emit('startGame',true);
         
     })
 	
